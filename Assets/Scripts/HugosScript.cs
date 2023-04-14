9e11b28757a4d640b3373ff20a839817e98f892e
Gift69 <gift2lp4kgmail.com>
2023-04-14 23:15:15 +0200

    lol

--- Assets/Scripts/HugosScript.cs
+++ Assets/Scripts/HugosScript.cs
@@ -6,20 +6,25 @@ using UnityEngine.Tilemaps;
 public class HugosScript : MonoBehaviour
 {
 
+    [SerializeField]
+    private float speed = 1;
+
     public Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
-private void OnCollisionStay2D(Collision2D other) {
-    Debug.Log(other.contactCount);
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("collision");
     }
 
-        
+
 }
